2f8616d5a2fac273610bc7efc0df01bc3adbf0a4
Steve Arnett <13557887+kg6zjl@users.noreply.github.com>
2019-05-31 12:51:46 -0700

    Update terraform.sh
    bump version and handle os type

--- modules/terraform.sh
+++ modules/terraform.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/bin
+VERSION="0.11.14"
+
 cd $HOME/bin
-curl -s -L -o $HOME/bin/terraform.zip https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_darwin_amd64.zip
+
+if [[ "$OSTYPE" == *"darwin"* ]]; then
+    curl -s -L -o $HOME/bin/terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_darwin_amd64.zip
+else
+    curl -s -L -o $HOME/bin/terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip
+fi
+
 unzip -o $HOME/bin/terraform.zip
 rm -f $HOME/bin/terraform.zip
