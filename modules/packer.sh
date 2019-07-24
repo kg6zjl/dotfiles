11d97dc29c00ea968e00a7bafc7b8a1f8961e111
Steve Arnett <13557887+kg6zjl@users.noreply.github.com>
2019-07-24 14:31:22 -0700

    Update packer.sh

--- modules/packer.sh
+++ modules/packer.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+RELEASE="1.0.0"
+
 mkdir $HOME/bin
-curl -sL -o $HOME/bin/packer.zip https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_darwin_amd64.zip
+curl -sL -o $HOME/bin/packer.zip https://releases.hashicorp.com/packer/${RELEASE}/packer_${RELEASE}_darwin_amd64.zip
 unzip -o $HOME/bin/packer.zip -d $HOME/bin
 rm -rf $HOME/bin/packer.zip
