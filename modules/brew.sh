b2619a8729ffe8073425d7d36a65134842b77dd7
Steve Arnett <13557887+kg6zjl@users.noreply.github.com>
2019-10-04 23:28:10 -0700

    Update brew.sh

--- modules/brew.sh
+++ modules/brew.sh
@@ -17,9 +17,13 @@ brew install gcc vpnc lz4 stoken gnutls
 brew install stoken
 
 #other misc brew installs
-brew install pwgen watch tree tmux node npm kubectl jq dep gcc llvm bash-completion maven osxfuse
+brew install grep pwgen watch tree tmux node npm kubectl jq dep gcc llvm bash-completion maven telnet jsonlint
 
-#brew installs for kvpn tool
+#install ruby and add to path
+brew install ruby
+
+#brew installs for kvpn/telepresence tool
+brew cask install osxfuse
 brew install datawire/blackbird/telepresence dos2unix
 
 #replace sed with gnu-sed, and symlink since --with-default-names is not available now
